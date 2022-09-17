1f4ebfa8fee5b373bfbe7759fdac0f681dcc3800
SSDD <0xSSDD@protonmail.com>
2022-09-18 00:56:05 +0300

    WIP:emit frens page

--- ui/pages/emit_frens/[tokenId].tsx
+++ ui/pages/emit_frens/[tokenId].tsx
@@ -1,11 +1,103 @@
-import { Heading } from '@chakra-ui/react'
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  VStack
+} from '@chakra-ui/react'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import {
+  useAccount,
+  // useContractEvent,
+  useContractWrite,
+  usePrepareContractWrite,
+  useWaitForTransaction
+} from 'wagmi'
 
-export default function Home() {
+import IPNFTContractABI from '../../abis/IPNFT.json'
+
+export default function EmitFren() {
+  const router = useRouter()
+  const { tokenId } = router.query
+  const { address, isConnected } = useAccount()
+
+  const [fractionsAmount, setFractionsAmount] = useState(1)
+  const [recipientAddress, setRecipientAddress] = useState('')
+  const [newTotalSupply, setNewTotalSupply] = useState(0)
+
+  const numFrensExisting = 0
+
+  const { config: contractWriteConfigForMint } = usePrepareContractWrite({
+    addressOrName: process.env.NEXT_PUBLIC_ERC1155_CONTRACT,
+    contractInterface: IPNFTContractABI,
+    functionName: 'setApprovalForAll',
+    args: [process.env.NEXT_PUBLIC_FRENCONSTITUTOR_CONTRACT, true]
+  })
+
+  const {
+    data: emitData,
+    write: setApprovalForAll,
+    isLoading: isEmitLoading,
+    isSuccess: isEmitStarted,
+    error: emitError
+  } = useContractWrite(contractWriteConfigForMint)
+  const {
+    data: txData,
+    isSuccess: txSuccess,
+    error: txError
+  } = useWaitForTransaction({
+    hash: emitData?.hash
+  })
+  const wasEmitted = txSuccess
+
+  useEffect(() => {
+    if (isConnected) {
+      setRecipientAddress(address)
+    }
+  }, [isConnected, address])
+
+  useEffect(() => {
+    setNewTotalSupply(numFrensExisting + fractionsAmount)
+  }, [fractionsAmount])
+
   return (
     <>
       <Heading as="h1" size="lg">
-        Emit FRENS tokens
+        Lock IP-NFT {tokenId} and create Fren tokens
       </Heading>
+
+      <Box mt={4}>
+        <VStack spacing={6}>
+          <FormControl>
+            <FormLabel>Amount of Frens to create</FormLabel>
+            <Input
+              variant="outline"
+              value={fractionsAmount}
+              onChange={(e) => setFractionsAmount(parseInt(e.target.value))}
+              type="number"
+              min={1}
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel>Recipient address</FormLabel>
+            <Input
+              variant="outline"
+              value={recipientAddress}
+              onChange={(e) => setRecipientAddress(e.target.value)}
+            />
+          </FormControl>
+
+          <p>New total supply will be {newTotalSupply}</p>
+
+          <Button colorScheme="green" onClick={setApprovalForAll as any}>
+            Create Fren tokens
+          </Button>
+        </VStack>
+      </Box>
     </>
   )
 }
