9f23a312c67ad9c4980f9e2148f40cd210305f9f
Schmackofant <schmackofant@protonmail.com>
2022-09-17 17:57:08 +0200

    check connection status

--- ui/pages/index.tsx
+++ ui/pages/index.tsx
@@ -1,10 +1,11 @@
-import { Heading } from "@chakra-ui/react";
+import { Heading } from '@chakra-ui/react'
 
 export default function Home() {
   return (
     <>
-      <Heading as='h1' size='lg'>
-        Hello World!<br/>
+      <Heading as="h1" size="lg">
+        Hello World!
+        <br />
         Welcome to the world of IP-NFT fractionalization
       </Heading>
     </>
--- ui/pages/ipnfts.tsx
+++ ui/pages/ipnfts.tsx
@@ -1,11 +1,23 @@
-import { Heading } from "@chakra-ui/react";
+import { Alert, AlertIcon, Box, Heading } from '@chakra-ui/react'
+import { useAccount } from 'wagmi'
 
 export default function Home() {
+  const { address, isConnected } = useAccount()
+
   return (
     <>
-      <Heading as='h1' size='lg'>
+      <Heading as="h1" size="lg">
         Your IP-NFTs
       </Heading>
+
+      <Box mt={2}>
+        {(!address || !isConnected) && (
+          <Alert colorScheme="red">
+            <AlertIcon />
+            Please connect your wallet to continue
+          </Alert>
+        )}
+      </Box>
     </>
   )
 }
