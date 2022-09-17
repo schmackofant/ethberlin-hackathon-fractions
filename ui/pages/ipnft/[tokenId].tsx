bc5fc2663c423d5607a865a2db872dd247f10346
Schmackofant <schmackofant@protonmail.com>
2022-09-17 19:14:01 +0200

    add a few page skeletons

---
+++ ui/pages/ipnft/[tokenId].tsx
@@ -0,0 +1,11 @@
+import { Heading } from '@chakra-ui/react'
+
+export default function Home() {
+  return (
+    <>
+      <Heading as="h1" size="lg">
+        View IP-NFT
+      </Heading>
+    </>
+  )
+}
