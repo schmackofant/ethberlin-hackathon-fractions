d2ac971b7d8a17df9c55160631c6b7a5b28e2dd8
Schmackofant <schmackofant@protonmail.com>
2022-09-18 02:13:04 +0200

    text update

--- ui/pages/index.tsx
+++ ui/pages/index.tsx
@@ -1,18 +1,25 @@
-import { Box,Divider, Heading, Image, Stack, Text} from '@chakra-ui/react'
+import { Box, Divider, Heading, Image, Stack, Text } from '@chakra-ui/react'
 
 export default function Home() {
   return (
     <>
       <Heading as="h1" size="lg">
-        Tokenized Intelecutal Property.
+        Tokenized Intellectual Property.
         <br />
       </Heading>
-      <Box boxSize='l' my="30px">
-  <Image src='/images/fractional.png' alt='Dan Abramov' borderRadius="5px"/>
-</Box>
-<Divider orientation='horizontal' marginBottom="30px"/>
-  <Text fontStyle="italic">Scientists must collect funds to conduct research. Patients should be empowered to fund therapeutics. Fractionalized IP-NFTs enable risk sharing partnerships for researchers, investors and patients.</Text>
-
+      <Box boxSize="l" my="30px">
+        <Image
+          src="/images/fractional.png"
+          alt="Dan Abramov"
+          borderRadius="5px"
+        />
+      </Box>
+      <Divider orientation="horizontal" marginBottom="30px" />
+      <Text fontStyle="italic">
+        Scientists must collect funds to conduct research. Patients should be
+        empowered to fund therapeutics. Fractionalized IP-NFTs enable risk
+        sharing partnerships for researchers, investors and patients.
+      </Text>
     </>
   )
 }
