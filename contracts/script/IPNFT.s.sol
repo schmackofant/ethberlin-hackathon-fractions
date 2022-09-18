7337b40074ec947db0aa99dea266178591dc8220
Schmackofant <schmackofant@protonmail.com>
2022-09-18 11:30:53 +0200

    Create IPNFT.s.sol

---
+++ contracts/script/IPNFT.s.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+import "forge-std/console.sol";
+import "../src/contracts/IPNFT.sol";
+import "../src/contracts/FRENConstitutor.sol";
+
+contract IPNFTScript is Script {
+    function setUp() public {}
+
+    function run() public {
+        vm.startBroadcast();
+        IPNFT nft = new IPNFT();
+        // deploy the FRENConstitutor contract with the right params
+        vm.stopBroadcast();
+    }
+}
+
+// - Run `anvil` to start local chain and get some accounts
+// - Put the first private key in .env file and run `source .env`
+// - Run `forge script script/IPNFT.s.sol:IPNFTScript --fork-url $ANVIL_RPC_URL --private-key $PRIVATE_KEY --broadcast -vvvv`
